53715503be3e3e3cc369573bfc59bd2792f63d99
Ahmed Fernandez Pardo <ahmedfernandezpardo@outlook.es>
2020-09-24 16:27:31 +0200

    Primer comentario

---
+++ PrimerGit/PrimerGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimerGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
